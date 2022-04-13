7015b6f4413fd0593088a6b6baf17cd963f0c628
DeLaRosaDev <61588726+DeLaRosaDev@users.noreply.github.com>
2022-04-12 23:37:59 -0400

    iniciamos el trabajo con Dependencies properties en los Custom UserControls

---
+++ Sadora/CustomElements/UscTextboxGeneral.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sadora.CustomElements
+{
+    /// <summary>
+    /// Lógica de interacción para UscTextboxGeneral.xaml
+    /// </summary>
+    public partial class UscTextboxGeneral : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(UscTextboxGeneral),
+                new PropertyMetadata(null));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxLengthProperty =
+            DependencyProperty.Register("MaxLength", typeof(int), typeof(UscTextboxGeneral),
+                new PropertyMetadata(0));
+
+        public int MaxLength
+        {
+            get { return (int)GetValue(MaxLengthProperty); }
+            set { SetValue(MaxLengthProperty, value); }
+        }
+        
+        public static readonly DependencyProperty BorderMarginProperty =
+            DependencyProperty.Register("BorderMargin", typeof(int), typeof(UscTextboxGeneral),
+                new PropertyMetadata(15));
+
+        public int BorderMargin
+        {
+            get { return (int)GetValue(BorderMarginProperty); }
+            set { SetValue(BorderMarginProperty, value); }
+        }
+
+        public UscTextboxGeneral()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
